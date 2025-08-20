271b23ab6fe916fd5d54fc3118901b1674b98777
simplescriptdev-lgtm <simplescriptdev@gmail.com>
2025-08-20 17:25:56 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php
-echo "<h1>Привіт! ✅ PHP працює</h1>";
+session_start();
+if (isset($_SESSION['user'])) {
+    header("Location: secret.php");
+    exit();
+}
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+<h2>Login</h2>
+<form method="post" action="login.php">
+    <label>Username:</label><input type="text" name="username"><br>
+    <label>Password:</label><input type="password" name="password"><br>
+    <button type="submit">Login</</button>
+</form>
+</body>
+</html>
