ab57c1a42703bd5435615de073cd3fa1e1d13439
simplescriptdev-lgtm <simplescriptdev@gmail.com>
2025-08-24 21:35:51 +0300

    Delete install.php

--- install.php
+++
@@ -1,7 +0,0 @@
-<?php
-$db = new SQLite3('users.db');
-$db->exec("CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY, username TEXT, password TEXT)");
-$db->exec("DELETE FROM users");
-$db->exec("INSERT INTO users(username,password) VALUES('admin','12345')");
-echo "Database created. User: admin / 12345";
-?>
